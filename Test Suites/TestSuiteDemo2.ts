52ca3bde1e097fac46dd84c0a039e59b50557abc
Daniel Nurrasyid <Daniel Nurrasyid@DanielsPC>
2022-07-31 10:59:52 +0700

    added TestSuiteDemo2

---
+++ Test Suites/TestSuiteDemo2.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteDemo2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2afe901f-1397-4250-9051-23c3aa0994b6</testSuiteGuid>
+   <testCaseLink>
+      <guid>434c65cb-2eab-4e89-993b-4113dd51ac07</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/RecordModeDemo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
