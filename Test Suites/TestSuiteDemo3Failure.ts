939cf5c59157550036577eb5b69374069a699525
Daniel Nurrasyid <Daniel Nurrasyid@DanielsPC>
2022-07-31 11:36:10 +0700

    added testsuitedemo3failure

---
+++ Test Suites/TestSuiteDemo3Failure.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteDemo3Failure</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>75084cae-9f9d-4ed3-a69d-9d6ee98ee337</testSuiteGuid>
+   <testCaseLink>
+      <guid>5209c343-25d8-4242-9e63-476a73b21007</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/ManualModeDemo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
